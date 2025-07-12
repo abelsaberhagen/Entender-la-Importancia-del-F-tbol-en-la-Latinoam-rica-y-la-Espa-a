a7b1b7958f110770399bb9a4e40ce587419812e7
abelsaberhagen <166773392+abelsaberhagen@users.noreply.github.com>
2025-07-11 21:05:36 -0400

    Add files via upload

---
+++ back.js
@@ -0,0 +1,5 @@
+const goBackButton = document.querySelector("#goBack")
+
+goBackButton.addEventListener("click", () => {
+    window.location.href = "futbol.html"
+});
---
+++ script.js
@@ -0,0 +1,21 @@
+const spainButton = document.querySelector("#spain")
+const mexicoButton = document.querySelector("#mexico")
+const argentinaButton = document.querySelector("#argentina")
+const uruguayButton = document.querySelector("#uruguay")
+
+// goBackButton.addEventListener("click", () => {
+//     window.location.href = "futbol.html"
+// });
+spainButton.addEventListener("click", () => {
+    window.location.href = "spain.html"
+});
+mexicoButton.addEventListener("click", () => {
+    //document.getElementByClass("imgButton").style.visibility = "hidden";
+    window.location.href = "mexico.html"
+});
+argentinaButton.addEventListener("click", () => {
+    window.location.href = "argentina.html"
+});
+uruguayButton.addEventListener("click", () => {
+    window.location.href = "uruguay.html"
+});
